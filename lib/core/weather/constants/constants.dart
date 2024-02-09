8f5ce9e5d8841c5bdda83a5997200e0edb729776
Oluwaferanmi-Michael <aechealgr8@gmail.com>
2024-02-08 23:54:47 -0700

    weather feature start

---
+++ lib/core/weather/constants/constants.dart
@@ -0,0 +1,8 @@
+
+class WeatherConstants{
+  const WeatherConstants._();
+
+  static const baseUrl = 'http://api.weatherapi.com/v1';
+  static const forcastUrl = 'forecast.json';
+  static const realtimeUrl = 'current.json';
+}
